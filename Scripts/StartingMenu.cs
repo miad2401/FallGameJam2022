04c04422bfb6d03b74ee1c45c28a8940fa278b23
Ninjajkl <ninjajkl@gmail.com>
2022-11-05 16:42:33 -0400

    Added A Sound Setting System
    Added A Sound Setting System

--- Scripts/StartingMenu.cs
+++ Scripts/StartingMenu.cs
@@ -1,18 +1,62 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class StartingMenu : Control
 {
+	Panel SettingsMenu;
+
+	Slider MusicSlider;
+	Slider WhiteNoiseSlider;
+	Slider SoundEffectsSlider;
+
+	List<Slider> sliderList = new List<Slider>();
+
+	float MusicVolumeDb;
+	float WhiteNoiseVolumeDb;
+	float SoundEffectsVolumeDb;
+
+	AudioStreamPlayer MusicPlayer;
+	AudioStreamPlayer SoundEffectsPlayer;
+	AudioStreamPlayer WhiteNoisePlayer;
+	public override void _Ready()
+    {
+		SettingsMenu = GetNode<Panel>("Settings");
+		SettingsMenu.Visible = false;
+
+		MusicPlayer = GetNode<AudioStreamPlayer>("MusicPlayer");
+		SoundEffectsPlayer = GetNode<AudioStreamPlayer>("SoundEffectsPlayer");
+		WhiteNoisePlayer = GetNode<AudioStreamPlayer>("WhiteNoisePlayer");
+
+		sliderList.Add(MusicSlider = SettingsMenu.GetNode<Slider>("SliderHolder/MusicSlider"));
+		sliderList.Add(WhiteNoiseSlider = SettingsMenu.GetNode<Slider>("SliderHolder/WhiteNoiseSlider"));
+		sliderList.Add(SoundEffectsSlider = SettingsMenu.GetNode<Slider>("SliderHolder/SoundEffectsSlider"));
+
+		foreach	(Slider slider in sliderList)
+        {
+			slider.MinValue = 0.0001;
+			slider.MaxValue = 1.5;
+			slider.Step = 0.0001;
+			slider.Value = 0.5;
+        }
+
+		loadSoundSettings("res://Sounds/soundSettings.tres");
+	}
 	private void OnStartButtonPressed()
 	{
-		GetTree().ChangeScene("res://Scenes/Main.tscn");
-	}
+		//soundSettings soundSettings = new soundSettings(2, WhiteNoiseVolumeDb, SoundEffectsVolumeDb);
+		var soundSettings = GD.Load<CSharpScript>("res://Scripts/soundSettings.cs").New(MusicVolumeDb, MusicSlider.Value, WhiteNoiseVolumeDb, WhiteNoiseSlider.Value, SoundEffectsVolumeDb, SoundEffectsSlider.Value);
 
-	private void OnSettingsButtonPressed()
-	{
-		//Open Settings
-	}
-	private void OnCreditsButtonPressed()
+		GD.Print(ResourceSaver.Save("res://Sounds/soundSettings.tres", soundSettings as soundSettings));
+		GetTree().ChangeScene("res://Scenes/Main.tscn");
+    }
+
+    private void OnSettingsButtonPressed()
+    {
+		SettingsMenu.Visible = true;
+		MusicPlayer.Stop();
+    }
+    private void OnCreditsButtonPressed()
 	{
 		//Open Credits
 	}
@@ -21,5 +65,76 @@ public class StartingMenu : Control
 	{
 		// quit game
 		GetTree().Quit();
+	}
+
+	private void OnExitSettingsButtonPressed()
+    {
+		SettingsMenu.Visible=false;
+		MusicPlayer.Play();
+    }
+
+	private void OnSoundSliderValueChanged(float linearValue, String name)
+    {
+		switch (name)
+        {
+			case "Music":
+				MusicVolumeDb = GD.Linear2Db(linearValue);
+				MusicPlayer.VolumeDb = MusicVolumeDb;
+				break;
+			case "WhiteNoise":
+				WhiteNoiseVolumeDb = GD.Linear2Db(linearValue);
+				WhiteNoisePlayer.VolumeDb = WhiteNoiseVolumeDb;
+				break;
+			case "SoundEffects":
+				SoundEffectsVolumeDb = GD.Linear2Db(linearValue);
+				SoundEffectsPlayer.VolumeDb = SoundEffectsVolumeDb;
+				break;
+		}
+    }
+
+	private void OnSliderDragStarted(String name)
+    {
+		switch (name)
+		{
+			case "Music":
+				MusicPlayer.Play();
+				break;
+			case "WhiteNoise":
+				WhiteNoisePlayer.Play();
+				break;
+			case "SoundEffects":
+				SoundEffectsPlayer.Play();
+				break;
+		}
+	}
+
+	private void OnSliderDragEnded(bool changed, String name)
+	{
+		switch (name)
+		{
+			case "Music":
+				MusicPlayer.Stop();
+				break;
+			case "WhiteNoise":
+				WhiteNoisePlayer.Stop();
+				break;
+			case "SoundEffects":
+				SoundEffectsPlayer.Stop();
+				break;
+		}
+	}
+
+	public void loadSoundSettings(String filePath)
+    {
+		soundSettings soundSettings = ResourceLoader.Load<soundSettings>(filePath);
+		MusicVolumeDb = soundSettings.MusicVolumeDb;
+		MusicPlayer.VolumeDb = MusicVolumeDb;
+		MusicSlider.Value = soundSettings.MusicSliderValue;
+		WhiteNoiseVolumeDb = soundSettings.WhiteNoiseVolumeDb;
+		WhiteNoisePlayer.VolumeDb = WhiteNoiseVolumeDb;
+		WhiteNoiseSlider.Value = soundSettings.WhiteNoiseSliderValue;
+		SoundEffectsVolumeDb = soundSettings.SoundEffectsVolumeDb;
+		SoundEffectsPlayer.VolumeDb = SoundEffectsVolumeDb;
+		SoundEffectsSlider.Value = soundSettings.SoundEffectsSliderValue;
 	}
 }
---
+++ Scripts/soundSettings.cs
@@ -0,0 +1,43 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+
+public class soundSettings : Resource
+{
+    [Export]
+    public float MusicVolumeDb { get; set; }
+
+    [Export]
+    public double MusicSliderValue { get; set; }
+
+    [Export]
+    public float WhiteNoiseVolumeDb { get; set; }
+
+    [Export]
+    public double WhiteNoiseSliderValue { get; set; }
+
+    [Export]
+    public float SoundEffectsVolumeDb { get; set; }
+
+    [Export]
+    public double SoundEffectsSliderValue { get; set; }
+
+    public soundSettings()
+    {
+        MusicVolumeDb = 0.5f;
+        MusicSliderValue = 0.5;
+        WhiteNoiseVolumeDb = 0.5f;
+        WhiteNoiseSliderValue = 0.5;
+        SoundEffectsVolumeDb = 0.5f;
+        SoundEffectsSliderValue = 0.5;
+    }
+    public soundSettings(float musicVolumeDb, double musicSliderValue, float whiteNoiseVolumeDb, double whiteNoiseSliderdouble ,float soundEffectsVolumeDb, double soundEffectsSliderValue)
+    {
+        MusicVolumeDb = musicVolumeDb;
+        MusicSliderValue = musicSliderValue;
+        WhiteNoiseVolumeDb = whiteNoiseVolumeDb;
+        WhiteNoiseSliderValue = whiteNoiseSliderdouble;
+        SoundEffectsVolumeDb = soundEffectsVolumeDb;
+        SoundEffectsSliderValue = soundEffectsSliderValue;
+    }
+}
