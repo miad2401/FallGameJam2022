c3b4ba7667ac31a0bb088b79aca8e92d28f1b891
Joey Parker <Ninjajkl@gmail.com>
2022-11-06 17:55:49 -0500

    Added Sound for Credits
    Added Sound for Credits

--- Scripts/Credits.cs
+++ Scripts/Credits.cs
@@ -1,9 +1,29 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Credits : Node2D
 {
     void _on_MainButton_pressed(){
         GetTree().ChangeScene("res://Scenes/StartingMenu.tscn");
+    }
+
+    List<Slider> sliderList = new List<Slider>();
+
+    float MusicVolumeDb;
+
+    AudioStreamPlayer MusicPlayer;
+    public override void _Ready()
+    {
+        MusicPlayer = GetNode<AudioStreamPlayer>("MusicPlayer");
+
+        loadSoundSettings("res://Sounds/soundSettings.tres");
+    }
+    public void loadSoundSettings(String filePath)
+    {
+        soundSettings soundSettings = ResourceLoader.Load<soundSettings>(filePath, null, true);
+        MusicVolumeDb = soundSettings.MusicVolumeDb;
+        MusicPlayer.VolumeDb = MusicVolumeDb;
+        MusicPlayer.Play();
     }
 }
